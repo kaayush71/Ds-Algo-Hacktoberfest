f64a5c4a62065095d03e4a1b2d6103e9449a94fe
SakshamJain7 <51989243+SakshamJain7@users.noreply.github.com>
2020-10-01 13:30:39 +0530

    Update bubblesort.py

--- Python/Sorting/bubblesort.py
+++ Python/Sorting/bubblesort.py
@@ -4,11 +4,8 @@ def bubblesort(list):
     for iter_num in range(len(list)-1,0,-1):
         for idx in range(iter_num):
             if list[idx]>list[idx+1]:
-                temp = list[idx]
-                list[idx] = list[idx+1]
-                list[idx+1] = temp
-
-
+                list[idx], list[idx+1] = list[idx+1], list[idx]
+    
 list = [19,2,31,45,6,11,121,27]
 bubblesort(list)
 print(list)
