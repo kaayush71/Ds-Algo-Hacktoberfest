61a008af842f1ef794b5d666a20078c6b7604ea5
Malavika_999 <65767896+malavika1699@users.noreply.github.com>
2020-10-04 17:08:53 +0530

    Update Palindrome.py

--- Python/Palindrome.py
+++ Python/Palindrome.py
@@ -1,9 +1,13 @@
 #to check whether a word is a palindrome
 
-s1=input("enter the word ").lower()
+s1=input("enter the word :").lower()
 r1=s1[::-1]
 
 if s1 == r1:
-	print("yes")
+	print("Pallindrome")
 else:
-	print("no")
+	print("Not pallindrome")
+
+print("Character at even index:")	
+for i in range(0,len(s1),2):
+    print(s1[i])
