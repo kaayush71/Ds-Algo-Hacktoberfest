42d123e5503c11f03a232f5d59debaef536ad125
Malavika_999 <65767896+malavika1699@users.noreply.github.com>
2020-10-04 20:08:30 +0530

    Create sort words.c

---
+++ Python/sort words.c
@@ -0,0 +1,13 @@
+my_str = input("Enter a string: ")
+
+# breakdown the string into a list of words
+words = my_str.split()
+
+# sort the list
+words.sort()
+
+# display the sorted words
+
+print("The sorted words are:")
+for word in words:
+   print(word)
