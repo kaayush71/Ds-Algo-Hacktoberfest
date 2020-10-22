2a00b8ff97553faa1f3c4c2ebe6d289ee04a470c
sriharikarthik08 <73272009+sriharikarthik08@users.noreply.github.com>
2020-10-22 15:24:24 +0530

    code is updated
    plz check the changes and run

--- Greedy algorithm prob. in pyhton/Money change/moneychng.py
+++ Greedy algorithm prob. in pyhton/Money change/moneychng.py
@@ -1,13 +1,32 @@
-n = int(input())
-count = 0
-while n>0:
-    if n >= 10:
-        count+=1
-        n = n-10
-    elif n>=5:
-        count+=1
-        n = n-5
-    elif n>=1:
-        count+=1
-        n=n-1
-print(count)
+import sys
+
+def get_change(m):
+    #write your code here
+    coins = [1, 3, 4]
+    cash = [0] * (m +1)
+    if m <= 1:
+        return m
+    cash[0] = 0
+    cash[1] = 1
+    #cash[3] = 1
+    #cash[4] = 1
+    coinreplacments = []
+    for i in range(2, m + 1):
+        for y in coins:
+            if i == y:
+                #cash[i] = 1
+                coinreplacments.append(1)
+            elif i > y:
+
+                coinreplacments.append(cash[i - y] + cash[y])
+            else:
+                pass
+
+        cash[i] = min(coinreplacments)
+        coinreplacments.clear()
+
+    return cash[m]
+
+if __name__ == '__main__':
+    m = int(sys.stdin.read())
+    print(get_change(m))
